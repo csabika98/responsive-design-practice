297d0c178f450640a70824da88e8e8c38d1841f7
Csaba Sallai <csaba.sallai@tinubu.com>
2026-03-30 14:05:21 +0200

    Add js logic

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,19 @@
 function main() {
-
     const addAnimalButton = document.getElementById("add-animal-button");
     const animalContainer = document.getElementById("animal-container");
+    
+    
+    const animalQuantityForm = document.getElementById("animal-quantity-form");
+    const animalQuantityInput = document.getElementById("animal-quantity");
 
     addAnimalButton.addEventListener("click", () => addRandomDog(animalContainer));
+    
+    
+    animalQuantityForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        await addAnimals(animalQuantityInput.valueAsNumber, animalContainer);
+        animalQuantityForm.reset();
+    });
 }
 
 async function addRandomDog(animalContainer) {
@@ -12,6 +22,12 @@ async function addRandomDog(animalContainer) {
     const image = document.createElement("img");
     image.src = data.message;
     animalContainer.appendChild(image);
+}
+
+async function addAnimals(animalQuantity, animalContainer) {
+    for (let animal = 0; animal < animalQuantity; animal += 1) {
+        await addRandomDog(animalContainer);
+    }
 }
 
 main();
