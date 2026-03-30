0eedad6a26ac07a8c039561753bc6eee1acb7cda
Csaba Sallai <csaba.sallai@tinubu.com>
2026-03-30 14:58:13 +0200

    Add some basic validation + some basic css styling

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,39 @@
 function main() {
     const addAnimalButton = document.getElementById("add-animal-button");
-    const animalContainer = document.getElementById("animal-container");
+    const dogsContainer = document.getElementById("dogs-container");
     
     
     const animalQuantityForm = document.getElementById("animal-quantity-form");
-    const animalQuantityInput = document.getElementById("animal-quantity");
+    const animalQuantityInput = document.getElementById("dogs-quantity-input");
+    const maxDogsQuantity = 10;
+    const minDogsQuantity = 1;
 
-    addAnimalButton.addEventListener("click", () => addRandomDog(animalContainer));
+    addAnimalButton.addEventListener("click", () => addRandomDog(dogsContainer));
     
     
     animalQuantityForm.addEventListener("submit", async (event) => {
         event.preventDefault();
-        await addAnimals(animalQuantityInput.valueAsNumber, animalContainer);
+        const animalQuantity = animalQuantityInput.valueAsNumber;
+        if (animalQuantity < minDogsQuantity || animalQuantity > maxDogsQuantity) {
+            return alert(`Please enter a number between ${minDogsQuantity} and ${maxDogsQuantity}`);
+        }
+        await addDogsQuantity(animalQuantity, dogsContainer);
+        // reset the form
         animalQuantityForm.reset();
     });
 }
 
-async function addRandomDog(animalContainer) {
+async function addRandomDog(dogsContainer) {
     const response = await fetch("https://dog.ceo/api/breeds/image/random");
     const data = await response.json();
     const image = document.createElement("img");
     image.src = data.message;
-    animalContainer.appendChild(image);
+    dogsContainer.appendChild(image);
 }
 
-async function addAnimals(animalQuantity, animalContainer) {
-    for (let animal = 0; animal < animalQuantity; animal += 1) {
-        await addRandomDog(animalContainer);
+async function addDogsQuantity(animalQuantity, dogsContainer) {
+    for (let dogs = 0; dogs < animalQuantity; dogs += 1) {
+        await addRandomDog(dogsContainer, dogs);
     }
 }
 
