013f1017c027a055e2e36b16de88063058b82237
Csaba Sallai <csaba.sallai@tinubu.com>
2026-03-30 13:40:56 +0200

    Add base fetch api + base css

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+function main() {
+
+    const addAnimalButton = document.getElementById("add-animal-button");
+    const animalContainer = document.getElementById("animal-container");
+
+    addAnimalButton.addEventListener("click", () => addRandomDog(animalContainer));
+}
+
+async function addRandomDog(animalContainer) {
+    const response = await fetch("https://dog.ceo/api/breeds/image/random");
+    const data = await response.json();
+    const image = document.createElement("img");
+
+    image.src = data.message;
+    
+
+    animalContainer.appendChild(image);
+
+}
+
+main();
